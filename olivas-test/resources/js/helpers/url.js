90692079038c1aa16044d339c66fc4bc496e587e
Marcos-SCO <marcos_sco@outlook.com>
2021-10-23 19:59:51 -0300

    mount query string

--- olivas-test/resources/js/helpers/url.js
+++ olivas-test/resources/js/helpers/url.js
@@ -1,9 +1,15 @@
 export const getUrl = () => window.location.href;
 
 export const getQueryParams = (params, url) => {
-  let href = url;
   // this is an expression to get query strings
   let regexp = new RegExp('[?&]' + params + '=([^&#]*)', 'i');
-  let qString = regexp.exec(href);
+  let qString = regexp.exec(url);
   return qString ? qString[1] : null;
 };
+
+export const mountQueryString = (data) => {
+  return Object.entries(data).reduce((acc, crr) => {
+    let [name, value] = crr;
+    return acc += `${name}=${value}`;
+  }, '');
+}
