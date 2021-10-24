07fe5e063a63c5ce85113558aceefeaa0cc288b0
Marcos-SCO <marcos_sco@outlook.com>
2021-10-24 13:11:10 -0300

    seller create request validation

--- olivas-test/app/Http/Requests/Seller/SellerCreate.php
+++ olivas-test/app/Http/Requests/Seller/SellerCreate.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests\Seller;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
 
 class SellerCreate extends FormRequest
 {
@@ -27,5 +30,22 @@ class SellerCreate extends FormRequest
             'name' => 'required',
             'trade_name' => 'required',
         ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Nome do vendedor é obrigatório',
+            'trade_name.required' => 'Nome fantasia é obrigatório'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Erros de validação',
+            'errors'      => $validator->errors()
+        ], Response::HTTP_NOT_ACCEPTABLE));
     }
 }
