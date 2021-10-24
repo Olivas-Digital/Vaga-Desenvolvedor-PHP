7588cf5ef8736f6585d44518441dde501b04473a
Marcos-SCO <marcos_sco@outlook.com>
2021-10-24 13:08:30 -0300

    convert obj to string

--- olivas-test/resources/js/helpers/strings.js
+++ olivas-test/resources/js/helpers/strings.js
@@ -1,3 +1,8 @@
 window.stringIncludes = (param, url) => {
   return url.includes(param);
 }
+
+window.convertObjToString = (obj, separator = '\n') => {
+  return Object.values(obj).
+    reduce((acc, crr) => acc += crr + separator, '');
+}
