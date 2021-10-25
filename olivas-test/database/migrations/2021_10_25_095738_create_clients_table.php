d385fa599f1c039eb607fa8aa0ff5da3e8cc84aa
Marcos-SCO <marcos_sco@outlook.com>
2021-10-25 10:08:17 -0300

    clients model controller factory migration

---
+++ olivas-test/database/migrations/2021_10_25_095738_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('email')->unique();
+            $table->string('image_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
--- olivas-test/database/seeders/DatabaseSeeder.php
+++ olivas-test/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        \App\Models\Seller::factory(5)->create();
+        // \App\Models\Seller::factory(5)->create();
+        \App\Models\Client::factory(5)->create();
     }
 }
