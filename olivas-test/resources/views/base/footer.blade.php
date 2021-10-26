a99c95109338311da06f6f3cc8a6efb2d1654d21
Marcos-SCO <marcos_sco@outlook.com>
2021-10-26 19:59:18 -0300

    base folder separation

---
+++ olivas-test/resources/views/base/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>&copy; Olivas Digital Test - Marcos</footer>
+<!-- Bootstrap js cdn -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+<!-- App -->
+<script src="{{asset('js/app.js')}}"></script>
--- olivas-test/resources/views/base/index.blade.php
+++ olivas-test/resources/views/base/index.blade.php
@@ -14,15 +14,13 @@
 </head>
 
 <body>
+  @include('base/header')
+
   <main class="main-container" data-page="{{$dataPage ?? ''}}">
     @yield('content')
   </main>
-
-  <footer>&copy; Olivas Digital Test - Marcos</footer>
-  <!-- Bootstrap js cdn -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
-  <!-- App -->
-  <script src="{{asset('js/app.js')}}"></script>
+  
+  @include('base/footer')
 </body>
 
 </html>
