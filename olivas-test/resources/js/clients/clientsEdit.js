0fa1e0385a02f111dcc5dd08be216ab1d7882d0c
Marcos-SCO <marcos_sco@outlook.com>
2021-10-25 22:00:58 -0300

    client edit test form

--- olivas-test/resources/js/clients/clientsEdit.js
+++ olivas-test/resources/js/clients/clientsEdit.js
@@ -35,25 +35,46 @@ const showClientsEditForm = (name, email) => {
   sendEditForm();
 }
 
+const createFormData = obj => {
+  let formData = new FormData();
+  return Object.entries(obj).forEach(item => {
+    let [key, value] = item;
+    console.log(key)
+    return formData.append(key, value);
+  });
+}
+
 const sendEditForm = () => {
   UISelect.clientEditForm().addEventListener('submit', (e) => {
     e.preventDefault()
     hideModalBootstrapModal('#client-edit-modal');
-
     let name = UISelect.clientFormName().value;
     let email = UISelect.clientFormEmail().value;
     let clientId = UISelect.clientItemActive().getAttribute('data-client-item-id');
 
     if (window.runQuery) return;
     window.runQuery = true;
-    axiosRequest('put', `${apiSelect.clientsPath}${clientId}`, {
+
+    let objData = {
+      _method: 'put',
       name: name,
-      email: email
-    })
+      email: email,
+      image: window.fileImgData,
+    };
+
+    //let formData = createFormData(objData);
+
+    // let formData = new FormData();
+    // formData.append('_method', 'put');
+    // formData.append('name', name);
+    // formData.append('email', email);
+    // formData.append('image', window.fileImgData);
+
+    axios.post(`${apiSelect.clientsPath}${clientId}`, formData, { headers: { 'content-type': 'multipart/form-data' } })
       // .then(console.log)
       .then((res) => {
         swal({
-          title: 'Opá, deu bom!', text: res.data.message, 
+          title: 'Opá, deu bom!', text: res.data.message,
           icon: "success",
           button: "Ok",
         }).then(() => fetchClientsResult());
