42c5e34abbb7566efcae9e09240753be0c642936
gilperon <95107329+gilperon@users.noreply.github.com>
2022-04-26 21:30:31 -0300

    Autenticacao JWT do zero, acessa criar-jwt, gera o token, e clica sobre o link para ser redirecionado pra login e fazer a busca

--- criar-jwt.php
+++ criar-jwt.php
@@ -18,10 +18,9 @@
 
             //Decodificar JWT e retornar em formato ARRAY
             $decode_jwt = json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $generate_jwt)[1]))),true);
-            echo "Nome da empresa: ".$decode_jwt["nome_empresa"];
+            echo "Nome da empresa: ".$decode_jwt["uname"];
             echo "<br><br><br>";
             var_dump($decode_jwt);
-
 
         } else {
             header("WWW-Authenticate: Basic realm=\"Private Area\"");
@@ -33,30 +32,31 @@
 
 
 
-    //Criar seu proprio token jwt
-    //Referencia -> https://dev.to/robdwaller/how-to-create-a-json-web-token-using-php-3gml )
-    //Decodificar o jwt aqui -> https://jwt.io/
     function criar_jwt(){
         
-        //Minha chave secreta para gerar o JWT -> De preferencia uma string forte
-        //$api_secret = "!abC123@"; 
-        $api_secret = "V2JHUzZsekZxdnZTUThBTGJPeGF0";
+        
+
+         $api_secret = "634c4fc187238c4b3e3f037a08c3ec0efcd3ca450bbc27a6188b5db4e477c74d";
+
+
+
 
         // Create token header as a JSON string
-        //$header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
-        // Create the token header
         $header = json_encode([
             'typ' => 'JWT',
             'alg' => 'HS256'
         ]);
 
-        // Create token payload as a JSON string
-        //$payload = json_encode(['user_id' => 12345,'nome_empresa' => 'SOMMA Investimentos','created'=>date('d/m/Y')]);
         $payload = json_encode([
-            'user_id' => 12345,
-            'nome_empresa' => 'Teste Git',
-            'created' => date('d/m/Y')
+            'sub' => 123, //ID do usuario tbm referenciado como user_id (opcional)
+            'uname' => 'Google Empresa', //Username eu que fiz (opcional)
+            'iss' => 'your.domain.name', // Issuer dominio de quem emitiu o token (opcional)
+            'iat' => time(), // Issued at: time when the token was generated (opcional)
+            'nbf' => time(), // Not before (OPCIONAL) a timestamp of when the token should start being considered valid. Should be equal to or greater than iat.
+            'exp' => time()+ 60 * 60 * 8, // Expire (unico parametro que nao é opcional, segundo o rfc, mas pode ser)
+            'key' => strtoupper(uniqid()) //(opcional)
         ]);
+        
 
         // Encode Header to Base64Url String
         $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
@@ -65,7 +65,6 @@
         $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
 
         // Create Signature Hash
-        //$signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, '!abC123@', true);
         $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $api_secret, true);
 
         // Encode Signature to Base64Url String
@@ -82,3 +81,15 @@
 
 
 ?>
+
+<br><br>
+<a href="verificar-jwt.php?jwt=<?php header("Authorization: Bearer $generate_jwt"); echo $generate_jwt; ?>">Logado</a>
+
+
+
+
+
+
+
+
+
